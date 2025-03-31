634a2017b082f6dfc721fd12d8ed039eb459cdb2
NestorEsk <nestoryahir2003@gmail.com>
2025-03-30 20:52:16 -0600

    Ajustes en la configuración de AXIOS, mejora e el manejo de errores y almacenamiento de usuario

--- src/api/services/authService.js
+++ src/api/services/authService.js
@@ -2,17 +2,19 @@ import axiosInstance from '../axiosConfig';
 import API_ENDPOINTS from '../apiEndpoints';
 
 export const login = async (credentials) => {
-    const response = await axiosInstance.post(API_ENDPOINTS.LOGIN, credentials);
+    const response = await axiosInstance.post(API_ENDPOINTS.AUTH.LOGIN, credentials);
     const { token, refreshToken, user } = response.data.data;
 
     // Guardar el token y el refreshToken en el almacenamiento local
     localStorage.setItem('jwtToken', token);
     localStorage.setItem('refreshToken', refreshToken);
+    localStorage.setItem('user', JSON.stringify(user)); // Guardar el usuario en el almacenamiento local
+    console.log(response);
 
     return user; // Retornar los datos del usuario
 };
 
 export const register = async (userData) => {
-    const response = await axiosInstance.post(API_ENDPOINTS.REGISTER, userData);
+    const response = await axiosInstance.post(API_ENDPOINTS.AUTH.REGISTER, userData);
     return response.data;
 };
--- src/api/utils/apiUtils.js
+++ src/api/utils/apiUtils.js
@@ -1,7 +1,7 @@
 export const handleApiError = (error) => {
     if (error.response) {
         // El servidor respondió con un estado fuera del rango 2xx
-        console.error('Error de respuesta:', error.response.data);
+        console.error('Error de respuesta:', error.response.data.message);
     } else if (error.request) {
         // La solicitud fue hecha pero no se recibió respuesta
         console.error('Error de solicitud:', error.request);
