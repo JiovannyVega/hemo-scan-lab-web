e987c8f8ed582039b35996ea5909da7a8f584d33
NestorEsk <nestoryahir2003@gmail.com>
2025-04-08 19:55:32 -0600

    Fix import path for PatientList component and update API endpoints to include person and analysis management

--- src/api/apiEndpoints.js
+++ src/api/apiEndpoints.js
@@ -15,6 +15,20 @@ const API_ENDPOINTS = {
         UPDATE_LAB: '/lab/update/:id', // Actualiza un laboratorio existente
         DELETE_LAB: '/lab/delete/:id', // Elimina un laboratorio
     },
+    PERSONS: {
+        GET_PERSONS: '/person', // Obtiene la lista de todas las personas
+        GET_PERSON_BY_ID: '/person/:id', // Obtiene una persona por su ID
+        CREATE_PERSON: '/person/create', // Crea una nueva persona
+        UPDATE_PERSON: '/person/update/:id', // Actualiza una persona existente
+        DELETE_PERSON: '/person/delete/:id', // Elimina una persona
+    },
+    ANALYSIS: {
+        GET_ANALYSIS: '/analysis', // Obtiene la lista de todos los análisis
+        GET_ANALYSIS_BY_ID: '/analysis/:id', // Obtiene un análisis por su ID
+        CREATE_ANALYSIS: '/analysis/create', // Crea un nuevo análisis
+        UPDATE_ANALYSIS: '/analysis/update/:id', // Actualiza un análisis existente
+        DELETE_ANALYSIS: '/analysis/delete/:id', // Elimina un análisis
+    },
 };
 
 export default API_ENDPOINTS;
